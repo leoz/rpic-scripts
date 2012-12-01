de1e1b47f9cfeeba3d81ee3a142c18ca56f4444a
Leonid Zolotarev <leonid.zolotarev@yahoo.com>
2012-11-30 21:12:16 -0800

    Correct turn logic

--- rpr_turn.sh
+++ rpr_turn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "### Turn!!! ###"
-i2cset -y 0 0x48 1 1700 w
-i2cset -y 0 0x48 2 1500 w
-i2cset -y 0 0x48 3 1500 w
-i2cset -y 0 0x48 4 1700 w
+i2cset -y 0 0x48 1 1300 w
+i2cset -y 0 0x48 2 1700 w
+i2cset -y 0 0x48 3 1700 w
+i2cset -y 0 0x48 4 1300 w
