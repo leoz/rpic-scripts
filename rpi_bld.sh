9061764564235c0854d1a9fdcdbcd8d869059cf2
leoz2011 <leoz.android@gmail.com>
2013-02-18 18:55:16 -0800

    Add video support

--- rpi_bld.sh
+++ rpi_bld.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
+
 mkdir rpic
 cd rpic
+
 git clone https://github.com/leoz2011/mongoose.git
 cd mongoose
 make linux
 cd ..
+
 git clone https://github.com/leoz2011/rpic-lib.git
 cd rpic-lib
 make
 cd ..
+
 git clone https://github.com/leoz2011/rpic-web.git
 cd rpic-web
 make
 cd ..
+
+git clone https://github.com/leoz2011/mjpg-streamer.git
+cd mjpg-streamer/mjpg-streamer
+make
+cd ../..
+
+git clone https://github.com/leoz2011/rpic-video.git
+
 cd ..
 
